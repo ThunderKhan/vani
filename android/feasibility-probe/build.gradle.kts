b989e65522bf19f1c049dba4149d5291a5f73041
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-09-20 13:42:23 +0530

    build(m4): configure Android instrumentation runner

--- android/feasibility-probe/build.gradle.kts
+++ android/feasibility-probe/build.gradle.kts
@@ -19,6 +19,7 @@ android {
         targetSdk = 36
         versionCode = 2
         versionName = "0.1.0-m4"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GIT_COMMIT", "\"$gitCommit\"")
     }
 
